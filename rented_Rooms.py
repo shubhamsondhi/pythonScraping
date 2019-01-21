0d479979e37e8474b050c304ecb27e2279df1201
Shubham Kumar <shubhamsondhi@gmail.com>
2019-01-21 06:17:58 -0500

    done

--- rented_Rooms.py
+++ rented_Rooms.py
@@ -3,53 +3,109 @@ import  urllib.request as urllib2
 import re
 import csv
 import os
-adds_list=[]
-items=[]
+import pprint
+import json
+# adds_list=[]
+# items=[]
+
+
+url1="https://www.kijiji.ca/b-short-term-rental/st-catharines/page-1/c42l80016"
+regex = r"(page-.)"
+# line = re.sub(regex, "page-"+str(1), url)
 
 def findDate(information):
-    b=(information.find('div',{'class',re.compile("datePosted")}).time is not None)
+    if information.find('div',{'class',re.compile("datePosted")}) is not None:
+        b=information.find('div',{'class',re.compile("datePosted")}).time is not None
+    else:
+        return "Not Present"       
+       # print(information)
     return  information.find('div',{'class',re.compile("datePosted")}).time["title"] if b else information.find('div',{'class',re.compile("datePosted")}).span["title"]
+
+
 def findPrice(information):
-    yield [x for x in information.find('span',{'class',re.compile("currentPrice")})]
+    # print("Showing Price")
+    price=information.find('span',{'class',re.compile("currentPrice")})
+    if(price is not None):
+        # print(price)
+        return price.text
+    
+    # yield [x for x in information.find('span',{'class',re.compile("currentPrice")})]
 def findAddress(information):
     yield information.find('span',{'class',re.compile("address")})
 def findDiscription(information):
     yield [x for x in information.find('div',{'class',re.compile("descriptionContainer")}).children]
 def findImages(information):
     yield [re.findall('url\(\"(.{2,})\"\)',x['style']) for x in information.findAll('div',{'class',re.compile("backgroundImage")})]
-def main():
-    for n in range(1,5):  
-        url='https://www.kijiji.ca/b-st-catharines/niagara-fall-room/page-'+str(n)+'/k0l80016?dc=true'
+
+
+
+def main(mainurl):
+    items=[]   
+    adds_list=[]
+    reg=r"/page.*" 
+    request = urllib2.urlopen(mainurl)    
+    soup2 = BeautifulSoup(request, 'html.parser')
+    totalAds=soup2.find('div',class_='showing').text
+    preTotal=re.sub(r".* of "," ",totalAds)
+    preTotal=re.sub(r" Ads.*"," ",preTotal)
+    numberOfPages=round(int(preTotal)/20)
+    if numberOfPages<5:
+        numberOfPages=numberOfPages+1
+    for n in range(1,numberOfPages+1):  
+        # url='https://www.kijiji.ca/b-st-catharines/niagara-fall-room/page-'+str(n)+'/k0l80016?dc=true'
+        url = re.sub(regex, "page-"+str(n), mainurl)
+        print(url)
+        print(n)
         response = urllib2.urlopen(url)
         soup = BeautifulSoup(response, 'html.parser')
-        #getting all the links----------
-        for n in soup.find_all('a',attrs={'class':'enable-search-navigation-flag'}, href=True):  
-            adds_list.append((n.text.strip(),n['href']))
-        for key, val in adds_list:    
-            items.append({'url':re.sub('(/b-st-catharines.{0,})',val,url)})
-        # print(items)
 
-def addInformation():    
-    for val in items:    
+        #getting all the links----------
+        for n in soup.find_all('a',class_='enable-search-navigation-flag', href=True):
+            if(n is not None):  
+                adds_list.append((n.text.strip(),n['href']))
+    for key, val in adds_list:
+        items.append({'url':"https://www.kijiji.ca"+val})
+    return items
+
+# bnsa= main(1,2,url)
+# bnsa[0]
+
+# m = numpy.array(items)
+# print (m.shape)
+
+def addInformation(fileName,items):
+    for val in items:  
+        # print(val)  
         request = urllib2.urlopen(val['url'])    
-        soup2 = BeautifulSoup(request, 'html.parser')
+        soup2 = BeautifulSoup(request, 'html.parser') 
         # print([(x[0].find('span')) for x in findPrice(soup2)])
-        val['price']=[x[0].text if x[0].find('span')!=-1 else 'Price not present' for x in findPrice(soup2)][0]
+        val['price']= findPrice(soup2)
         val['address']= [x.text for x in findAddress(soup2)][0]
-        # # val['discription']= [x[1].text for x in findDiscription(soup2)][0]
+        val['discription']= [x[1].text for x in findDiscription(soup2)][0]
         val['images']= [x for x in findImages(soup2)][0] 
         val['date']=findDate(soup2)   
     try:
-
-        with open('C:/Users/shubh/Downloads/Data/niagaraFallRooms.csv', "w", newline='') as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames = [n for n in val.keys()])         
-                writer.writeheader()   
-                for val in items:     
-                    writer.writerows([val])
+        with open('C:/Users/shubh/Desktop/working/'+fileName+'.csv', "w", newline='') as csv_file: 
+            writer = csv.DictWriter(csv_file, fieldnames = [n for n in val.keys()])                    
+            writer.writeheader()
+            writer.writerows([val])                    
     except:
         print("error")
-main()
-addInformation()
+
+items=main(url1)
+len(items)
+listOfUrl=[]
+for k in items:
+    print(k["url"])
+    listOfUrl.append(k["url"])
+
+len(listOfUrl)
+with open('data.json', 'w') as outfile:
+    json.dump(items, outfile)
+
+
+
+addInformation("Short term Room",items)
 # print(items)
 # request = urllib2.urlopen(items[0]['url'])    
 # soup2 = BeautifulSoup(request, 'html.parser')
